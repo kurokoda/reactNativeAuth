2bd1c247e7ad86ef7b5c0731c3149fc8d79bab5d
Kurokoda <indigo.developer@gmail.com>
2019-01-09 14:27:23 -0500

    Adds Firebase

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Header } from './src/components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDGK6tAzNmT5UgNGzG0xEcRdZBcFf4RPOI',
+      authDomain: 'reactnativeauth-8e36c.firebaseapp.com',
+      databaseURL: 'https://reactnativeauth-8e36c.firebaseio.com',
+      projectId: 'reactnativeauth-8e36c',
+      storageBucket: 'reactnativeauth-8e36c.appspot.com',
+      messagingSenderId: '311378830273'
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header text="Authentication" />
+        <Header text='Authentication' />
         <Text>This is an app.</Text>
       </View>
     );
