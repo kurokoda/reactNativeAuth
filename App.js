5a73e12f727a4953947df775e5bec5ce188fa511
Kurokoda <indigo.developer@gmail.com>
2019-01-09 14:13:21 -0500

    Adds common components

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header } from './src/components/common';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header text="Authentication" />
         <Text>This is an app.</Text>
       </View>
     );
