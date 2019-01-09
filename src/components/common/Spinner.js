55287348a68d793be5b40ae4a4fa1118e1ff658c
Kurokoda <indigo.developer@gmail.com>
2019-01-09 17:01:30 -0500

    Adds Spinner component

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Spinner = () => {
+
+  const { containerStyle } = styles;
+
+  return (
+        <View style={containerStyle} />
+  )
+};
+
+const styles = {
+  containerStyle: {
+
+  }
+}
+
+export { Spinner }
