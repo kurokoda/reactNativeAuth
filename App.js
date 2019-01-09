ad79510c18ab2210c8b1cd5e6cd3bad066b3d69d
Kurokoda <indigo.developer@gmail.com>
2019-01-09 14:03:35 -0500

    Initializing App component

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>This is an app.</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({});
