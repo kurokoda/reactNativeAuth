06d7d0c83ac10056381020442b8f433c5d499b1b
Kurokoda <indigo.developer@gmail.com>
2019-01-09 14:41:00 -0500

    Adds text input functionality

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 import { Button, Card, CardSection } from '../common'
+
 class LoginForm extends Component {
+  state = {
+    text: ''
+  };
+
   render () {
     return (
         <Card>
-          <CardSection />
-            <TextInput style={{ height: 20, width: 100 }} />
+          <CardSection>
+            <TextInput
+                value={this.state.text}
+                onChangeText={ text => this.setState({ text })}
+                style={{ height: 20, width: 100 }} />
+          </CardSection>
           <CardSection />
           <CardSection>
             <Button>Log in</Button>
